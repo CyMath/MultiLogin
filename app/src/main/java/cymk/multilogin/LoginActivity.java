e6efff5dad1d4e7823e953cb5fdaf67e484b1aa5
Cyril Mathew <cyrilm13@gmail.com>
2017-06-13 02:08:56 -0500

    Added Twitter Login

--- app/src/main/java/cymk/multilogin/LoginActivity.java
+++ app/src/main/java/cymk/multilogin/LoginActivity.java
@@ -6,9 +6,15 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 
 import com.facebook.login.widget.LoginButton;
+import com.twitter.sdk.android.core.DefaultLogger;
+import com.twitter.sdk.android.core.Twitter;
+import com.twitter.sdk.android.core.TwitterAuthConfig;
+import com.twitter.sdk.android.core.TwitterConfig;
+import com.twitter.sdk.android.core.identity.TwitterLoginButton;
 
 public class LoginActivity extends AppCompatActivity
 {
@@ -16,15 +22,24 @@ public class LoginActivity extends AppCompatActivity
     //Facebook Login Object
     FBLogin fbLogin;
 
+    //Twitter Login Object
+    TwitterLogin tLogin;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
 
+        //Twitter must be initialized BEFORE the button is inflated.
+        tLogin = new TwitterLogin(this);
+        setContentView(R.layout.activity_login);
 
         //Facebook
         LoginButton FBLoginButton = (LoginButton) findViewById(R.id.fb_login_button);
         fbLogin = new FBLogin(FBLoginButton, this);
+
+        //Twitter Button
+        TwitterLoginButton twtLoginButton = (TwitterLoginButton) findViewById(R.id.twitter_loginButton);
+        tLogin.setLoginButton(twtLoginButton);
 
     }
 
@@ -34,6 +49,9 @@ public class LoginActivity extends AppCompatActivity
 
         //Send result of activity to Facebook Login, if it was facebook.
         fbLogin.loginResult(requestCode, resultCode, data);
+
+        //Send result of activity to Twitter Login, if it was Twitter
+        tLogin.loginResult(requestCode, resultCode, data);
     }
 
     //Don't want the user to be able to go back from the login
--- app/src/main/java/cymk/multilogin/TwitterLogin.java
+++ app/src/main/java/cymk/multilogin/TwitterLogin.java
@@ -1,8 +1,61 @@
 package cymk.multilogin;
 
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+import com.twitter.sdk.android.core.Callback;
+import com.twitter.sdk.android.core.DefaultLogger;
+import com.twitter.sdk.android.core.Result;
+import com.twitter.sdk.android.core.Twitter;
+import com.twitter.sdk.android.core.TwitterAuthConfig;
+import com.twitter.sdk.android.core.TwitterConfig;
+import com.twitter.sdk.android.core.TwitterException;
+import com.twitter.sdk.android.core.TwitterSession;
+import com.twitter.sdk.android.core.identity.TwitterLoginButton;
+
 /**
  * Created by Cyril Mathew on 6/13/2017.
  */
 
-public class TwitterLogin {
+class TwitterLogin
+{
+    private TwitterLoginButton loginButton;
+    private Context context;
+
+    TwitterLogin(Context context)
+    {
+        this.context = context;
+        TwitterConfig config = new TwitterConfig.Builder(context)
+                .logger(new DefaultLogger(Log.DEBUG))
+                .twitterAuthConfig(new TwitterAuthConfig("qkd3jbgu1LpnrdyNvklVEJmha", "JYy5ujxZMBBarIfZ4dUNjBuK1RA9SK8QDkyoUOEyT5gawmb4kW"))
+                .debug(true)
+                .build();
+        Twitter.initialize(config);
+
+    }
+
+    void setLoginButton(TwitterLoginButton button)
+    {
+        loginButton = button;
+
+        loginButton.setCallback(new Callback<TwitterSession>() {
+            @Override
+            public void success(Result<TwitterSession> result) {
+                // Do something with result, which provides a TwitterSession for making API calls
+                ((LoginActivity)context).loginFinish();
+            }
+
+            @Override
+            public void failure(TwitterException exception) {
+                // Do something on failure
+            }
+        });
+    }
+
+    void loginResult(int requestCode, int resultCode, Intent data)
+    {
+        loginButton.onActivityResult(requestCode, resultCode, data);
+    }
+
 }
